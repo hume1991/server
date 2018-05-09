17337b74becfb3d045987482a7f9549c2bba3a88
Georg Ehrke <developer@georgehrke.com>
2018-05-09 12:05:46 +0200

    make sure force language is reflected in html lang attribute
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- lib/private/L10N/Factory.php
+++ lib/private/L10N/Factory.php
@@ -130,6 +130,13 @@ class Factory implements IFactory {
 			return $this->requestLanguage;
 		}
 
+		$forceLang = $this->config->getSystemValue('force_language', false);
+		if (is_string($forceLang)) {
+			if ($this->languageExists($app, $forceLang)) {
+				return $forceLang;
+			}
+		}
+
 		/**
 		 * At this point Nextcloud might not yet be installed and thus the lookup
 		 * in the preferences table might fail. For this reason we need to check
